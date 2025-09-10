63f09296147af4ea2ad2580df1fcfadefe9fcda3
LGeneral <leogame0819@gmail.com>
2025-09-10 23:04:27 +0800

    feature: change root

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  base: "./", // 使用相對路徑
+  build: {
+    outDir: "dist", // 輸出目錄為 dist
+    assetsDir: "assets", // 資源檔案目錄為 assets
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
